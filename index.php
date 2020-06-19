c1cbb5782d44f8b75c6204a24a0e775fd9912bf5
Tai <66994879+Doantai47@users.noreply.github.com>
2020-06-19 22:56:30 +0700

    Update index.php

--- index.php
+++ index.php
@@ -92,7 +92,7 @@
                     <h3>Sign In</h3>
                 </div>
                 <div class="card-body">
-                    <form action="connect.php" method="POST">
+                    <form action="view.html" method="POST">
                         <div class="input-group form-group">
                             <div class="input-group-prepend">
                                 <span class="input-group-text"><i class="fas fa-user"></i></span>
